d9cdf049434730da9a375fcb7fa0d8432003931c
evgeny <evgeny@MacBook-Air-evgeny.local>
2021-11-23 03:17:06 +0300

    prefirst on review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,14 +8,6 @@ class IsOwnerOrReadOnly(BasePermission):
         return obj.author == request.user
 
 
-# class IsAdminUser(BasePermission):
-#     def has_permission(self, request, view):
-#         user = request.user
-#         if user.is_anonymous:
-#             return False
-#         return bool(user and user.is_admin)
-
-
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         user = request.user
@@ -28,20 +20,3 @@ class IsAdminOrReadOnly(BasePermission):
             request.user.is_superuser
             or request.user.role == request.user.ADMIN
         )
-
-
-# class IsAuthorOrAdminOrModerator(BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.method == "POST":
-#             return not request.user.is_anonymous()
-
-#         if request.method in ("PATCH", "DELETE"):
-#             return (
-#                 request.user == obj.author
-#                 or request.user.role == request.user.ADMIN
-#                 or request.user.role == request.user.MODERATOR
-#             )
-
-#         if request.method in SAFE_METHODS:
-#             return True
-#         return False
